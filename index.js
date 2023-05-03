73e07082efc5a9928fc1b94ab18cf1d3da7c2761
Tasneem Nur <tasneemnurtashnia@gmail.com>
2023-05-03 20:48:11 +0600

    A json file created giving the necessary info about chefs.

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+const chefs = require('./data/chefs.json')
+app.get("/", (req, res)=>{
+res.send("server is running")
+});
+
+app.get("/chefs", (req, res)=>{
+res.send(chefs)
+});
+
+app.get("/chefs/id", (req, res)=>{
+res.send(chefs)
+});
+
+app.listen(port, ()=>{
+console.log(`project is running on port: ${port}`)
+});
+
+
